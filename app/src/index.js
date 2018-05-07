d710f568dd0fc51da710908ff76174110cfbe8ad
Christian Franco <contact@francoslab.com>
2018-05-06 17:55:26 -0700

    add prisma bindings

--- app/src/index.js
+++ app/src/index.js
@@ -1,35 +1,31 @@
 const { GraphQLServer } = require('graphql-yoga');
+const { Prisma } = require('prisma-binding');
 
-let links = [{
-	id: 'link-0',
-	url: 'www.howtographql.com',
-	description: 'Fullstack tutorial for GraphQL'
-}];
-
-let idCount = links.length
 const resolvers = {
 	Query: {
 		info: () => `This is the API of a Hackernews Clone`,
-		feed: () => links,
+		feed: (root, args, context, info) => {
+			return context.db.query.links({}, info);
+		},
 	},
 	Mutation: {
-		post: (root, args) => {
-			const link = {
-				id: `link-${idCount++}`,
-				description: args.description,
-				url: args.url,
-			}
-			links.push(link);
-			return link;
+		post: (root, args, context, info) => {
+			console.log(root, args, context, info);
+			return context.db.mutation.createLink({
+				data: {
+					url: args.url,
+					description: args.description,
+				},
+			}, info);
 		},
 		updateLink: (root, args) => {
 			const { id, url, description } = args;
 			if (!id) {
 				return null;
 			}
-			for(let i = 0; i < links.length; i++) {
+			for (let i = 0; i < links.length; i++) {
 				const workingLink = links[i];
-				if(workingLink.id === id) {
+				if (workingLink.id === id) {
 					links[i] = {
 						...workingLink,
 						url: url || workingLink.url,
@@ -47,7 +43,7 @@ const resolvers = {
 			}
 			let deletedLink = null;
 			links = links.filter(link => {
-				if(link.id === id) {
+				if (link.id === id) {
 					deletedLink = link;
 				}
 				return link.id !== id
@@ -65,6 +61,15 @@ const resolvers = {
 const server = new GraphQLServer({
 	typeDefs: './src/schema.graphql',
 	resolvers,
+	context: req => ({
+		...req,
+		db: new Prisma({
+			typeDefs: 'src/generated/prisma.graphql',
+			endpoint: 'http://localhost:4466/hackernews/dev',
+			secret: 'awesome1337secret',
+			debug: true,
+		}),
+	}),
 });
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
