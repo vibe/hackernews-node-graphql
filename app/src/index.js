ab8a50ae7648436198b45f61fe753825c1702cf9
Christian Franco <contact@francoslab.com>
2018-05-06 18:34:33 -0700

    add authentication

--- app/src/index.js
+++ app/src/index.js
@@ -1,62 +1,14 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { Prisma } = require('prisma-binding');
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const AuthPayload = require('./resolvers/AuthPayload');
 
 const resolvers = {
-	Query: {
-		info: () => `This is the API of a Hackernews Clone`,
-		feed: (root, args, context, info) => {
-			return context.db.query.links({}, info);
-		},
-	},
-	Mutation: {
-		post: (root, args, context, info) => {
-			console.log(root, args, context, info);
-			return context.db.mutation.createLink({
-				data: {
-					url: args.url,
-					description: args.description,
-				},
-			}, info);
-		},
-		updateLink: (root, args) => {
-			const { id, url, description } = args;
-			if (!id) {
-				return null;
-			}
-			for (let i = 0; i < links.length; i++) {
-				const workingLink = links[i];
-				if (workingLink.id === id) {
-					links[i] = {
-						...workingLink,
-						url: url || workingLink.url,
-						description: description || workingLink.description,
-					}
-					return links[i];
-				}
-			}
-			return null;
-		},
-		deleteLink: (root, args) => {
-			const { id, url, description } = args;
-			if (!id) {
-				return null;
-			}
-			let deletedLink = null;
-			links = links.filter(link => {
-				if (link.id === id) {
-					deletedLink = link;
-				}
-				return link.id !== id
-			});
-			return deletedLink;
-		}
-	},
-	Link: {
-		id: (root) => root.id,
-		description: root => root.description,
-		url: root => root.url,
-	}
-}
+	Query,
+	Mutation,
+	AuthPayload,
+};
 
 const server = new GraphQLServer({
 	typeDefs: './src/schema.graphql',
